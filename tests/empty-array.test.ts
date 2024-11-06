82c7779bb68030a41f47bdbe5154a63566eee81f
Niamh <niamh.linguist@gmail.com>
2024-11-06 18:27:50 +0200

    pre-commit hook, formatting and linting

--- tests/empty-array.test.ts
+++ tests/empty-array.test.ts
@@ -1,13 +1,12 @@
-import { test, expect } from '@playwright/test';
-import {StatusCodes} from "http-status-codes";
-let baseURL: string = 'http://localhost:3000/users';
+import { test, expect } from '@playwright/test'
+import { StatusCodes } from 'http-status-codes'
+const baseURL: string = 'http://localhost:3000/users'
 
 test.describe('User management API', () => {
-
-    test('GET / - should return empty when no users', async ({ request }) => {
-        const response = await request.get(`${baseURL}`);
-        expect(response.status()).toBe(StatusCodes.OK);
-        const responseBody = await response.text()
-        expect(responseBody).toBe('[]');
-    });
-});
+  test('GET / - should return empty when no users', async ({ request }) => {
+    const response = await request.get(`${baseURL}`)
+    expect(response.status()).toBe(StatusCodes.OK)
+    const responseBody = await response.text()
+    expect(responseBody).toBe('[]')
+  })
+})
--- tests/user-controller.test.ts
+++ tests/user-controller.test.ts
@@ -1,63 +1,60 @@
 // tests/api.spec.ts
-import { test, expect } from '@playwright/test';
-import {StatusCodes} from "http-status-codes";
-let baseURL: string = 'http://localhost:3000/users';
+import { test, expect } from '@playwright/test'
+import { StatusCodes } from 'http-status-codes'
+const baseURL: string = 'http://localhost:3000/users'
 
 type User = {
-    id: number,
-    name: string,
-    phone: string,
-    email: string
+  id: number
+  name: string
+  phone: string
+  email: string
 }
 
 test.describe('User management API', () => {
+  let createdUserId: number
+  let deletedUserId: number
+  let createdUser: User
+  const nonExistingUserId = 1000
 
-    let createdUserId: number
-    let deletedUserId: number
-    let createdUser: User
-    const nonExistingUserId = 1000
+  test('POST / - should add a new user', async ({ request }) => {
+    const response = await request.post(`${baseURL}`)
+    expect.soft(response.status()).toBe(StatusCodes.CREATED)
 
-    test('POST / - should add a new user', async ({ request }) => {
-        const response = await request.post(`${baseURL}`);
-        expect.soft(response.status()).toBe(StatusCodes.CREATED);
+    createdUser = await response.json()
+    createdUserId = createdUser.id
 
-        createdUser = await response.json()
-        createdUserId = createdUser.id
+    const usersResponse = await request.get(`${baseURL}`)
+    const users: User[] = await usersResponse.json()
 
+    const user = users.find((user: User) => user.id === createdUserId)
+    expect.soft(user).toEqual(createdUser)
+  })
 
-        const usersResponse = await request.get(`${baseURL}`);
-        const users: User[] = await usersResponse.json()
+  test('GET /:id - should return a user by ID', async ({ request }) => {
+    const response = await request.get(`${baseURL}/${createdUserId}`)
+    expect.soft(response.status()).toBe(StatusCodes.OK)
+    const responseBody = await response.json()
+    expect.soft(responseBody.id).toBe(createdUserId)
+  })
 
-        const user = users.find((user: User) => user.id === createdUserId)
-        expect.soft(user).toEqual(createdUser)
-    });
+  test('GET /:id - should return 404 if user not found', async ({ request }) => {
+    const response = await request.get(`${baseURL}/${nonExistingUserId}`)
+    expect.soft(response.status()).toBe(StatusCodes.NOT_FOUND)
+  })
 
-    test('GET /:id - should return a user by ID', async ({ request }) => {
-        const response = await request.get(`${baseURL}/${createdUserId}`);
-        expect.soft(response.status()).toBe(StatusCodes.OK);
-        const responseBody = await response.json()
-        expect.soft(responseBody.id).toBe(createdUserId);
-    });
+  test('DELETE /:id - should delete a user by ID', async ({ request }) => {
+    const firstGetResponse = await request.get(`${baseURL}/${createdUserId}`)
+    expect.soft(firstGetResponse.status()).toBe(StatusCodes.OK)
 
-    test('GET /:id - should return 404 if user not found', async ({ request }) => {
-        const response = await request.get(`${baseURL}/${nonExistingUserId}`);
-        expect.soft(response.status()).toBe(StatusCodes.NOT_FOUND);
-    });
+    await request.delete(`${baseURL}/${createdUserId}`)
+    deletedUserId = createdUserId
 
-    test('DELETE /:id - should delete a user by ID', async ({ request }) => {
+    const secondGetResponse = await request.get(`${baseURL}/${deletedUserId}`)
+    expect.soft(secondGetResponse.status()).toBe(StatusCodes.NOT_FOUND)
+  })
 
-        const firstGetResponse = await request.get(`${baseURL}/${createdUserId}`);
-        expect.soft(firstGetResponse.status()).toBe(StatusCodes.OK);
-
-        await request.delete(`${baseURL}/${createdUserId}`);
-        deletedUserId = createdUserId
-
-        const secondGetResponse = await request.get(`${baseURL}/${deletedUserId}`);
-        expect.soft(secondGetResponse.status()).toBe(StatusCodes.NOT_FOUND);
-    });
-
-    test('DELETE /:id - should return 404 if user not found', async ({ request }) => {
-        const response = await request.delete(`${baseURL}/${deletedUserId}`);
-        expect.soft(response .status()).toBe(StatusCodes.NOT_FOUND);
-    });
-});
+  test('DELETE /:id - should return 404 if user not found', async ({ request }) => {
+    const response = await request.delete(`${baseURL}/${deletedUserId}`)
+    expect.soft(response.status()).toBe(StatusCodes.NOT_FOUND)
+  })
+})
