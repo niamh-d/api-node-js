5bc5f5e17fa7b687da58b285e8a7167a144448cb
Niamh <niamh.linguist@gmail.com>
2024-11-06 18:17:20 +0200

    refactoring and renaming of variables

--- tests/user-controller.test.ts
+++ tests/user-controller.test.ts
@@ -1,48 +1,63 @@
 // tests/api.spec.ts
 import { test, expect } from '@playwright/test';
+import {StatusCodes} from "http-status-codes";
 let baseURL: string = 'http://localhost:3000/users';
+
+type User = {
+    id: number,
+    name: string,
+    phone: string,
+    email: string
+}
 
 test.describe('User management API', () => {
 
-    let existingUserId: number
+    let createdUserId: number
+    let deletedUserId: number
+    let createdUser: User
+    const nonExistingUserId = 1000
 
     test('POST / - should add a new user', async ({ request }) => {
         const response = await request.post(`${baseURL}`);
-        expect.soft(response.status()).toBe(201);
-        const returnedUser = await response.json()
-        existingUserId = returnedUser.id
+        expect.soft(response.status()).toBe(StatusCodes.CREATED);
+
+        createdUser = await response.json()
+        createdUserId = createdUser.id
+
 
         const usersResponse = await request.get(`${baseURL}`);
-        const users = await usersResponse.json()
+        const users: User[] = await usersResponse.json()
 
-        const user = users.find((user: { id: number; }) => user.id === existingUserId)
-        expect.soft(user).toEqual(returnedUser)
+        const user = users.find((user: User) => user.id === createdUserId)
+        expect.soft(user).toEqual(createdUser)
     });
 
     test('GET /:id - should return a user by ID', async ({ request }) => {
-        const response = await request.get(`${baseURL}/${existingUserId}`);
-        expect.soft(response.status()).toBe(200);
+        const response = await request.get(`${baseURL}/${createdUserId}`);
+        expect.soft(response.status()).toBe(StatusCodes.OK);
         const responseBody = await response.json()
-        expect.soft(responseBody.id).toBe(existingUserId);
+        expect.soft(responseBody.id).toBe(createdUserId);
     });
 
     test('GET /:id - should return 404 if user not found', async ({ request }) => {
-        const id = 50
-        const response = await request.get(`${baseURL}/${id}`);
-        expect.soft(response.status()).toBe(404);
+        const response = await request.get(`${baseURL}/${nonExistingUserId}`);
+        expect.soft(response.status()).toBe(StatusCodes.NOT_FOUND);
     });
 
     test('DELETE /:id - should delete a user by ID', async ({ request }) => {
 
-        const firstGetResponse = await request.get(`${baseURL}/${existingUserId}`);
-        expect.soft(firstGetResponse.status()).toBe(200);
-        await request.delete(`${baseURL}/${existingUserId}`);
-        const secondGetResponse = await request.get(`${baseURL}/${existingUserId}`);
-        expect.soft(secondGetResponse.status()).toBe(404);
+        const firstGetResponse = await request.get(`${baseURL}/${createdUserId}`);
+        expect.soft(firstGetResponse.status()).toBe(StatusCodes.OK);
+
+        await request.delete(`${baseURL}/${createdUserId}`);
+        deletedUserId = createdUserId
+
+        const secondGetResponse = await request.get(`${baseURL}/${deletedUserId}`);
+        expect.soft(secondGetResponse.status()).toBe(StatusCodes.NOT_FOUND);
     });
 
     test('DELETE /:id - should return 404 if user not found', async ({ request }) => {
-        const response = await request.delete(`${baseURL}/${existingUserId}`);
-        expect.soft(response .status()).toBe(404);
+        const response = await request.delete(`${baseURL}/${deletedUserId}`);
+        expect.soft(response .status()).toBe(StatusCodes.NOT_FOUND);
     });
 });
